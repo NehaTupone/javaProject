244588a793983679e5ea404776f0fae875f6d47b
NehaTupone <92578887+NehaTupone@users.noreply.github.com>
2024-11-13 02:04:01 +0530

    Update Demo.java

--- DemoB181/src/Demo.java
+++ DemoB181/src/Demo.java
@@ -4,6 +4,10 @@ public class Demo {
 	{
 		System.out.println("This is m1 method");
 	}
+	public void m2()
+	{
+		System.out.println("This is m2 method");
+	}
 public static void main(String[] args) {
 	System.out.println("Git demo method");
 }
